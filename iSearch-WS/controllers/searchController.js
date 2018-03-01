93dab73a6b55b6d2e1d150994fdd6a4926746eff
Yoav Saroya <stankovic100@gmail.com>
2018-03-01 12:25:10 +0200

    Starting the Search Algorithm

--- iSearch-WS/controllers/searchController.js
+++ iSearch-WS/controllers/searchController.js
@@ -5,12 +5,35 @@ const Consts  = require('../consts');
 const Documents = require('../models/documents'),
       Terms = require('../models/terms');
 
+let cleanQuerySearch = (querySearch) => {
+
+    // LowerCase the querySearch
+    querySearch = querySearch.toLowerCase();
+
+    // remove double spaces
+    querySearch = querySearch.replace(/ +(?= )/g,'');
+
+    return querySearch;
+};
+
 exports.search = (req, res, next) => {
 
     // Getting the query search string
     let querySearch = req.query['querySearch'];
 
+    // Clean the query search
+    querySearch = cleanQuerySearch(querySearch);
+
+    let arrayOfWords = querySearch.split(" ");
+
+    Terms.find({'word': {$in: arrayOfWords}})
+        .then( (words) => {
+           console.log(words);  // words from DB which is in the QuerySearch
+        });
+
+
     //TODO: Deep Dive into Searching Algorithm...
+
 
     res.json("Search Works");
 };
