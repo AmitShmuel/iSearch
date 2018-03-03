1a16cc91e229b0d860fba0501440a79de71ea12c
Yoav Saroya <stankovic100@gmail.com>
2018-03-03 19:21:29 +0200

    Making Progress in boolean Operators

--- iSearch-WS/controllers/searchController.js
+++ iSearch-WS/controllers/searchController.js
@@ -3,7 +3,8 @@ const StopList = require('../data/stoplist'),
       Soundex = require('../bl/soundex'),
       Stemmer = require('en-stemmer'),
       Consts  = require('../consts'),
-      isLetter = require('is-letter');
+      isLetter = require('is-letter'),
+      _ = require('lodash');
 
 // Models
 const Documents = require('../models/documents'),
@@ -11,6 +12,21 @@ const Documents = require('../models/documents'),
 
 let lowerCaseAndRemoveDoubleSpaces = (text) => text.toLowerCase().replace(/\s+/g, ' ');
 
+let unionArrays = (x, y) => {
+    let obj = {};
+    for (let i = x.length-1; i >= 0; -- i)
+        obj[x[i]] = x[i];
+    for (let i = y.length-1; i >= 0; -- i)
+        obj[y[i]] = y[i];
+    let res = [];
+    for (let k in obj) {
+        if (obj.hasOwnProperty(k))  // <-- optional
+            res.push(obj[k]);
+    }
+    return res;
+};
+
+//TODO: not support ""
 let isQuotationMarksBalanced = (text) => {
 
     let isBalanced = true;
@@ -23,15 +39,30 @@ let isQuotationMarksBalanced = (text) => {
     return isBalanced;
 };
 
+let removeReduntdantOperators = (text) => {
+    let index = 0;
+    while((index = text.indexOf(Consts.AND_SIGN, index)) !== -1) {
+        let isNotOperandAfterAnd = text[index + 1] === Consts.NOT_SIGN || text[index + 2] === Consts.NOT_SIGN;
+        if(isNotOperandAfterAnd) {
+            text = text.substr(0, index) + text.substr(index).replace(Consts.AND_SIGN, '');
+        }
+        index++;
+    }
+    return text;
+};
+
 let checkOperand = (text, operand) => {
 
     let index = 0;
     while((index = text.indexOf(operand, index)) !== -1) {
 
-        let condition = !isLetter(text[index + 1]) && !isLetter(text[index + 2]);
+        let condition = (text[index + 1] !== '"' && !isLetter(text[index + 1]))
+            && (text[index + 2] !== '"' && !isLetter(text[index + 2]));
+
         condition = operand === Consts.NOT_SIGN ?
             condition :
-            condition || (!isLetter(text[index - 1]) && !isLetter(text[index - 2]));
+            condition || ( ( text[index - 1] !== '"' && !isLetter(text[index - 1]) )
+            && ( text[index - 2] !== '"' && !isLetter(text[index - 2]) ) );
 
         if(condition) return false;
 
@@ -41,9 +72,10 @@ let checkOperand = (text, operand) => {
 };
 
 
-let isOperandsIsCorrect = (text) => {
+let isOperandsCorrect = (text) => {
 
     let tmpText = lowerCaseAndRemoveDoubleSpaces(text);
+    tmpText = removeReduntdantOperators(tmpText);
 
     return checkOperand(tmpText, Consts.NOT_SIGN)
         && checkOperand(tmpText, Consts.OR_SIGN)
@@ -71,20 +103,44 @@ let cleanQuerySearch = (querySearch) => {
     // LowerCase && remove double spaces
     querySearch = lowerCaseAndRemoveDoubleSpaces(querySearch);
 
+    querySearch = removeReduntdantOperators(querySearch);
+
+
+    let expressions = [];
     let wordsOnly = querySearch.match(/\b(\w+)\b/g);
+    let operatorsOnly = querySearch.match(/&|!|(\|)/g);
 
     if(wordsOnly != null) {
-        // Clear words which belong to StopList && is not between " "
-        wordsOnly = wordsOnly.filter( w =>
-            !( StopList.indexOf(w) > -1 && !isBetweenQuotationMarks(w, originalQuerySearch)) );
-
         // Stemming the words
         for(let i = 0; i < wordsOnly.length; i++) {
             wordsOnly[i] = Stemmer.stemmer(wordsOnly[i]);
         }
+
+        if(operatorsOnly != null) {
+            for (let i = 0; i < operatorsOnly.length; i++) {
+                let expression = {};
+
+                expression.operator = operatorsOnly[i];
+                expression.leftHand = wordsOnly[i];
+                expression.rightHand = wordsOnly[i + 1];
+
+                expressions.push(expression);
+            }
+        }
+
+        // Clear words which belong to StopList && is not between " "
+        wordsOnly = wordsOnly.filter( w =>
+            !( StopList.indexOf(w) > -1 && !isBetweenQuotationMarks(w, originalQuerySearch)) );
+
+        expressions = expressions.filter( e =>
+            !( wordsOnly.indexOf(e.rightHandStem) > -1) );
     }
 
-    return wordsOnly;
+    return {
+        words: wordsOnly,
+        expressions: expressions
+    }
+    // return wordsOnly;
 };
 
 let generateSoundexCodes = (words) => {
@@ -114,21 +170,28 @@ exports.search = (req, res, next) => {
     }
 
     // Check Operands is correct
-    if(!isOperandsIsCorrect(querySearch)) {
+    if(!isOperandsCorrect(querySearch)) {
         res.status(500).json("Operands are not correct");
         return;
     }
 
     // Clean the query search - lowercase, empty spaces, special characters, Stop list, Stemming
-    let arrayOfWords = cleanQuerySearch(querySearch);
-    if(arrayOfWords == null) {
+    // let arrayOfWords = cleanQuerySearch(querySearch);
+    // if(arrayOfWords == null) {
+    //     res.status(500).json("Search field must contain at least one word");
+    //     return;
+    // }
+
+    let queryObj = cleanQuerySearch(querySearch);
+    if(queryObj.words == null) {
         res.status(500).json("Search field must contain at least one word");
         return;
     }
 
     let whereObject = {
         $or: [
-            {'word': {$in: arrayOfWords}},
+            // {'word': {$in: arrayOfWords}},
+            {'word': {$in: queryObj.words}},
         ],
         //$and: [ // Should represent the boolean operands
         //    //{'word': {$ne:'then' }}, // NOT
@@ -143,49 +206,74 @@ exports.search = (req, res, next) => {
     };
 
     if(isSoundexActivated) {
-        let arrayOfSoundexCodes = generateSoundexCodes(arrayOfWords);
+        // let arrayOfSoundexCodes = generateSoundexCodes(arrayOfWords);
+        let arrayOfSoundexCodes = generateSoundexCodes(queryObj.words);
         whereObject.$or.push({'soundexCode': {$in: arrayOfSoundexCodes}});
     }
 
     Terms.find(whereObject)
         .populate({path: 'locations.document', model: Documents})
-        .then((words) => {
-            // docs => words from DB which is in the QuerySearch + Documents with FULL DATA
-            let documents = {}; // Key => ID of the document, Value => The Document with full data
+        .then((words) => { // words => words from DB which is in the QuerySearch + Documents with FULL DATA
 
-            for(let word of words) {
-                for(let location of word._doc.locations) {
-                    let documentId = location._doc.document._doc._id;
-                    if(documents[documentId] == null) {
-                        documents[documentId] = location._doc.document._doc;
-                        //documents[documentId].words = [word._doc];
+            let resultDocuments = null;
+
+            // No Operators
+            if(queryObj.expressions.length === 0) {
+                let documents = {}; // Key => ID of the document, Value => The Document with full data
+                for(let word of words) {
+                    for(let location of word._doc.locations) {
+                        let documentId = location._doc.document._doc._id;
+                        if(documents[documentId] == null) {
+                            documents[documentId] = location._doc.document._doc;
+                        }
                     }
-                    //else {
-                        //documents[documentId].words.push(word._doc);
-                    //}
+                }
+                resultDocuments = documents;
+            }
+
+            // Operands
+            else {
+                let wordsObj = {};
+                for(let word of words) {
+                    wordsObj[word._doc.word] = [];
+                    for(let location of word._doc.locations) {
+                        wordsObj[word._doc.word].push(location._doc.document._doc);
+                    }
+                }
+                //sort
+                //queryObj.expressions = queryObj.expressions.sort( (a,b) => {
+                //    if(a.operator === Consts.AND_SIGN) return a;
+                //    else if(? 1 : -1);
+                //
+                //});
+                queryObj.expressions = _.orderBy(queryObj.expressions, ['operator'],['asc']); // Use Lodash to sort array by 'name'
+
+
+                resultDocuments = wordsObj[queryObj.expressions[0].leftHand];
+
+                for(let i = 0; i < queryObj.expressions.length; i ++) {
+                    let rightWord = queryObj.expressions[i].rightHand;
+                    let docsOfRightWord = wordsObj[rightWord];
+
+                    switch(queryObj.expressions[i].operator) {
+                        case Consts.AND_SIGN:
+                            resultDocuments = _.intersection(resultDocuments, docsOfRightWord);
+                            break;
+
+                        case Consts.OR_SIGN:
+                            resultDocuments = _.union(resultDocuments, docsOfRightWord);
+                            break;
+
+                        case Consts.NOT_SIGN:
+                            resultDocuments = _.difference(resultDocuments, docsOfRightWord);
+                            break;
+
+                        default: break;
+                    }
                 }
             }
 
-            // Operands start here (?)
-            // Start Test
-            let wordsObj = {};
-            for(let word of words) {
-                wordsObj[word._doc.word] = [];
-                for(let location of word._doc.locations) {
-                    wordsObj[word._doc.word].push(location._doc.document._doc._id);
-                }
-            }
-
-            
-
-            /// End of Test
-
-            // Clearing disabled documents
-            let documentsArray = Object.values(documents).filter(doc => doc.isActive);
-
-            // Cleaning words from documents
-            //documentsArray.forEach(doc => delete doc.words );
-
-            res.json(documentsArray);
+            resultDocuments = Object.values(resultDocuments).filter(doc => doc.isActive);
+            res.json(resultDocuments);
         });
 };
